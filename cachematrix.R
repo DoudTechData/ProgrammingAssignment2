8ae3896f480588b6d4612f3467a4650eced162ff
Edouard Chevalier <edouard.chevalier@techmydata.net>
2015-01-24 10:53:04 +0100

    adding comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-##Wraps a matrix with an object that can store its inverse. The inverse should be provided by calling functions.
+##Wraps a matrix with an object that can store its inverse.
+##The inverse should be provided by calling functions.
 makeCacheMatrix <- function(x = matrix()) {
     inverse <- NULL
     set <- function(y) {
@@ -17,7 +18,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-##Retrieve the inverse stored in a cached matrix object (see makeCacheMatrix). If not computed yet, compute and store it.
+##Retrieve the inverse stored in a cached matrix object (see makeCacheMatrix).
+##If not computed yet, compute and store it.
 cacheSolve <- function(x, ...) {
     inv <- x$getInverse()
     if(!is.null(inv)) {
